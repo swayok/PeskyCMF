9733f7a6cad926f02c41927729d1af24306e1d41
Filippov Aleksandr <swayalex@gmail.com>
2016-04-12 18:54:09 +0300

    Added AdminAuthorised event prototype

---
+++ src/PeskyCMF/Event/AdminAuthorised.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace PeskyCMF\Event;
+
+use App\Events\Event;
+use Illuminate\Queue\SerializesModels;
+
+class AdminAuthorised extends Event {
+
+    use SerializesModels;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct() {
+        //
+    }
+
+    /**
+     * Get the channels the event should be broadcast on.
+     *
+     * @return array
+     */
+    public function broadcastOn() {
+        return [];
+    }
+}
