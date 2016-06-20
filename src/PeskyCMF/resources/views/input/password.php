99c4db05854e6f0072c3ff61120eb3a87f7d8b5e
Sway <swayalex@gmail.com>
2016-06-20 22:22:35 +0300

    cmf::input.password - hidden fields moved outside of .form-group so they do not show error messages when login/email input fails validation

--- src/PeskyCMF/resources/views/input/password.php
+++ src/PeskyCMF/resources/views/input/password.php
@@ -26,13 +26,13 @@ $attributesForEdit = \Swayok\Html\Tag::buildAttributes(array_merge($attributes, 
     {{? !!it.isCreation }}
 <?php endif; ?>
 
+<!-- disable chrome email & password autofill -->
+<input type="text" name="login" class="hidden" formnovalidate disabled>
+<input type="password" class="hidden" formnovalidate disabled>
+<input type="text" name="email" class="hidden" formnovalidate value="test@test.com" disabled>
+<input type="password" class="hidden" formnovalidate disabled>
+<!-- end of autofill disabler -->
 <div class="form-group">
-    <!-- disable chrome email & password autofill -->
-    <input type="text" name="login" class="hidden" formnovalidate disabled>
-    <input type="password" class="hidden" formnovalidate disabled>
-    <input type="text" name="email" class="hidden" formnovalidate value="test@test.com" disabled>
-    <input type="password" class="hidden" formnovalidate disabled>
-    <!-- end of autofill disabler -->
     <label for="<?php echo $attributes['id']; ?>"><?php echo $fieldConfig->getLabel(); ?></label>
     <input {{? !!it.isCreation }}<?php echo $attributesForCreate; ?>{{??}}<?php echo $attributesForEdit; ?>{{?}}>
 </div>
