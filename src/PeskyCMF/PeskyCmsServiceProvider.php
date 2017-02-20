3313ca34d447f035533359c80898db943531cdef
Filippov Aleksandr <swayalex@gmail.com>
2017-02-20 18:03:25 +0300

    PeskyCmsServiceProvider - db classes registration refactoring (now each class has own method to make it easier to owerwrite it)

--- src/PeskyCMF/PeskyCmsServiceProvider.php
+++ src/PeskyCMF/PeskyCmsServiceProvider.php
@@ -26,49 +26,105 @@ class PeskyCmsServiceProvider extends PeskyCmfServiceProvider {
         // note: scaffolds declared in CmsSiteLoader
     }
 
+    // admins
+
     public function registerAdminsDbClasses() {
+        $this->registerAdminsDbRecord();
+        $this->registerAdminsDbTable();
+        $this->registerAdminsDbTableStructure();
+    }
+
+    public function registerAdminsDbRecord() {
         $this->app->bind(CmsAdmin::class, function () {
             return CmsAdmin::newEmptyRecord();
         });
+    }
+
+    public function registerAdminsDbTable() {
         $this->app->singleton(CmsAdminsTable::class, function () {
             return CmsAdminsTable::getInstance();
         });
+    }
+
+    public function registerAdminsDbTableStructure() {
         $this->app->singleton(CmsAdminsTableStructure::class, function () {
             return CmsAdminsTableStructure::getInstance();
         });
     }
 
+    // settings
+
     public function registerSettingsDbClasses() {
+        $this->registerSettingsDbRecord();
+        $this->registerSettingsDbTable();
+        $this->registerSettingsDbTableStructure();
+    }
+
+    public function registerSettingsDbRecord() {
         $this->app->bind(CmsSetting::class, function () {
             return CmsSetting::newEmptyRecord();
         });
+    }
+
+    public function registerSettingsDbTable() {
         $this->app->singleton(CmsSettingsTable::class, function () {
             return CmsSettingsTable::getInstance();
         });
+    }
+
+    public function registerSettingsDbTableStructure() {
         $this->app->singleton(CmsSettingsTableStructure::class, function () {
             return CmsSettingsTableStructure::getInstance();
         });
     }
 
+    // pages
+
     public function registerPagesDbClasses() {
+        $this->registerPagesDbRecord();
+        $this->registerPagesDbTable();
+        $this->registerPagesDbTableStructure();
+    }
+
+    public function registerPagesDbRecord() {
         $this->app->bind(CmsPage::class, function () {
             return CmsPage::newEmptyRecord();
         });
+    }
+
+    public function registerPagesDbTable() {
         $this->app->singleton(CmsPagesTable::class, function () {
             return CmsPagesTable::getInstance();
         });
+    }
+
+    public function registerPagesDbTableStructure() {
         $this->app->singleton(CmsPagesTableStructure::class, function () {
             return CmsPagesTableStructure::getInstance();
         });
     }
 
+    // texts
+
     public function registerTextsDbClasses() {
+        $this->registerTextsDbRecord();
+        $this->registerTextsDbTable();
+        $this->registerTextsDbTableStructure();
+    }
+
+    public function registerTextsDbRecord() {
         $this->app->bind(CmsText::class, function () {
             return CmsText::newEmptyRecord();
         });
+    }
+
+    public function registerTextsDbTable() {
         $this->app->singleton(CmsTextsTable::class, function () {
             return CmsTextsTable::getInstance();
         });
+    }
+
+    public function registerTextsDbTableStructure() {
         $this->app->singleton(CmsTextsTableStructure::class, function () {
             return CmsTextsTableStructure::getInstance();
         });
