c0b84612bfdb6a1b51df327e2a4c0c3638afcffc
Sway <swayalex@gmail.com>
2017-04-06 00:03:57 +0300

    CmsPage - removed setter for dropped column; CmsTextsMigration - fixed

--- src/PeskyCMF/CMS/Texts/CmsTextsMigration.php
+++ src/PeskyCMF/CMS/Texts/CmsTextsMigration.php
@@ -33,7 +33,6 @@ class CmsTextsMigration extends Migration {
                     $table->text('custom_info');
                 }
 
-                $table->index('parent_id');
                 $table->index('created_at');
                 $table->index('updated_at');
                 $table->unique(['page_id', 'language']);
