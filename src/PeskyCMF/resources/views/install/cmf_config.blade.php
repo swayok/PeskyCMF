27dd7459f6aeeb6406bbfc1b6071ee2aa86f5423
Sway <swayalex@gmail.com>
2017-12-07 17:03:51 +0300

    Fixed cmf_config.blade.php

--- src/PeskyCMF/resources/views/install/cmf_config.blade.php
+++ src/PeskyCMF/resources/views/install/cmf_config.blade.php
@@ -54,7 +54,7 @@ class {{ $cmfCongigClassName }} extends CmfConfig {
                     ]
                 ]*/
             ],
-            static::$menuItems
+            static::getMenuItems()
         );
     }
 
