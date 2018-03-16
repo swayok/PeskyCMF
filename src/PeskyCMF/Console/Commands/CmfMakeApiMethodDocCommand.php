e7ba3f3851fb9edda0f1195f85da85668a92135c
Filippov Aleksandr <swayalex2@gmail.com>
2018-03-16 16:46:55 +0300

    API method documentation class creation command upgraded to auto-fill URL parameters and ask for url query and POST parameters in order to generate almost finished documentation class

--- src/PeskyCMF/Console/Commands/CmfMakeApiMethodDocCommand.php
+++ src/PeskyCMF/Console/Commands/CmfMakeApiMethodDocCommand.php
@@ -57,17 +57,17 @@ class CmfMakeApiMethodDocCommand extends Command {
         $url = $this->guessUrl($translationGroup === 'method' ? null : $translationGroup, $translationSubGroup);
         $translationGroup .= '.' . $translationSubGroup;
         $httpMethod = 'GET';
-        $httpMethodUrlQueryParam = '//        \'_method\' => \'PUT\',';
-        $postParams = '';
         if (!$this->option('auto') && $this->confirm('Do you want to configure documentation class?', true)) {
-            list($url, $httpMethod, $translationGroup) = $this->askQuestions($url, $httpMethod, $translationGroup);
+            list($url, $httpMethod, $translationGroup) = $this->askQuestions($url, $translationGroup);
+            list($urlParams, $urlQueryParams, $postParams, $validationErrors) = $this->askParams($url, $httpMethod);
             if (!in_array($httpMethod, ['GET', 'POST'], true)) {
-                $httpMethodUrlQueryParam = "        '_method' => '$httpMethod',";
                 $httpMethod = $httpMethod === 'PUT' ? 'POST' : 'GET';
             }
-            if ($httpMethod === 'POST') {
-                $postParams = "public \$postParameters = [\n        'id' => 'int'\n    ];\n";
-            }
+        } else {
+            $postParams = "    public \$postParameters = [\n        //'key' => 'value',\n    ];\n";
+            $urlParams = "    public \$urlParameters = [\n        //'key' => 'value',\n    ];\n";
+            $urlQueryParams = "    public \$urlQueryParameters = [\n        //'_method' => 'PUT',\n    ];\n";
+            $validationErrors = "    public \$validationErrors = [\n        //'key' => 'required|string',\n    ];\n";
         }
 
         $fileContents = <<<CLASS
@@ -90,18 +90,9 @@ class {$className} extends {$baseClassName} {
         'Accept-Language' => '{{language}}',
         'Authorization' => 'Bearer {{auth_token}}'
     ];
-    public \$urlParameters = [
-//        'url_parameter' => 'int'
-    ];
-    public \$urlQueryParameters = [
-$httpMethodUrlQueryParam
-    ];
-    $postParams
-    protected \$validationErrors = [
-//        'token' => ['required', 'string'],
-//        'id' => ['required', 'integer', 'min:1']
-    ];
-
+{$urlParams}{$urlQueryParams}{$postParams}
+{$validationErrors}
+    
     public \$onSuccess = [
 //        'name' => 'string',
     ];
@@ -125,7 +116,7 @@ $httpMethodUrlQueryParam
 }
 CLASS;
         File::save($filePath, $fileContents, 0644, 0755);
-        $this->line('File created');
+        $this->line("File $filePath created");
         $this->line('Add next translations to you dictionaries:');
         $translations = [];
         array_set($translations, $translationGroup . '.title', '');
@@ -133,9 +124,9 @@ CLASS;
         $this->line($this->arrayToString($translations));
     }
 
-    protected function askQuestions($url, $httpMethod, $translationGroup) {
-        $url = $this->ask('API method url:', $url);
-        $httpMethod = $this->askWithCompletion('HTTP method:', ['GET', 'POST', 'PUT', 'DELETE'], $httpMethod);
+    protected function askQuestions($url, $translationGroup) {
+        $url = $this->ask('API method url (use "{param}" to mark variable part of URL):', $url);
+        $httpMethod = $this->choice('HTTP method:', ['GET', 'POST', 'PUT', 'DELETE'], 0);
         $translationGroup = $this->ask('Translation path:', $translationGroup);
         return [$url, $httpMethod, $translationGroup];
     }
@@ -151,5 +142,57 @@ CLASS;
             }
         }
         return $ret . str_pad('', $depth * 4, ' ') . "],\n";
+    }
+
+    protected function askParams($url, $httpMethod) {
+        $urlParams = [];
+        $urlQueryParams = [];
+        $postParams = '';
+        // url params
+        if (preg_match_all('%\{([^}]+)\}%', $url, $matches)) {
+            foreach ($matches[1] as $paramName) {
+                $urlParams[$paramName] = 'string';
+            }
+        }
+        $urlParams = '    public $urlParameters = ' . rtrim($this->arrayToString($urlParams, 1), " ,\n") . ";\n";
+        // url query params
+        if (!in_array($httpMethod, ['GET', 'POST'], true)) {
+            $urlQueryParams['_method'] = $httpMethod;
+        }
+        list($urlQueryParams, $validationErrors) = $this->askParamsFor('URL Query', $urlQueryParams);
+        $urlQueryParams = '    public $urlQueryParameters = ' . rtrim($this->arrayToString($urlQueryParams, 1), " ,\n") . ";\n";
+        // post params
+        if (in_array($httpMethod, ['POST', 'PUT'], true)) {
+            list($postParams, $validationErrors) = $this->askParamsFor('POST');
+            $postParams = '    public $postParameters = ' . rtrim($this->arrayToString($postParams, 1), " ,\n") . ";\n";
+        }
+        $validationErrors = '    protected $validationErrors = ' . rtrim($this->arrayToString($validationErrors, 1), " ,\n") . ";\n";
+        return [$urlParams, $urlQueryParams, $postParams, $validationErrors];
+    }
+
+    protected function askParamsFor($type, array $predefinedParams = []) {
+        $validationErrors = [];
+        $params = $predefinedParams;
+        if ($this->confirm("Do you want to provide $type parameters?", true)) {
+            $this->line('Format: "param_name:comment" (ex: "id:integer, required") or "parent.key:comment" to nest params');
+            if (!empty($predefinedParams)) {
+                $this->line('Predefined parameters: ' . $this->arrayToString($predefinedParams, 0));
+            }
+            do {
+                $param = $this->ask("Input $type parameter or press Enter to stop");
+                if (trim($param) === '') {
+                    break;
+                }
+                $parts = explode(':', $param, 2);
+                if (count($parts) !== 2) {
+                    $this->error('Invalid parameter format. "param_name:comment" expected');
+                    continue;
+                }
+                array_set($params, $parts[0], $parts[1]);
+                array_set($validationErrors, $parts[0], 'required|string');
+                $this->line('Parameters: ' . $this->arrayToString($params));
+            } while (trim($param) !== '');
+        }
+        return [$params, $validationErrors];
     }
 }
