b4d32eb43cbc2805cf243d58312510ccbc66025b
Filippov Aleksandr <swayalex2@gmail.com>
2018-04-28 15:08:20 +0300

    Refactored styling to better fit top navbar presence especially for black light skin

---
+++ src/PeskyCMF/resources/views/ui/top_navbar.blade.php
@@ -0,0 +1,14 @@
+<?php
+/*
+<nav class="navbar navbar-static-top">
+    <div class="navbar-custom-menu">
+        <ul class="nav navbar-nav">
+            <li>
+                <a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a>
+            </li>
+        </ul>
+    </div>
+
+</nav>
+ */
+?>
