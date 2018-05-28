bdcd2a795b2126a025d37dd1a7d889034c51f292
Filippov Aleksandr <swayalex2@gmail.com>
2018-05-28 13:42:54 +0300

    HttpCode::NOT_ACCEPTABLE added

--- src/PeskyCMF/HttpCode.php
+++ src/PeskyCMF/HttpCode.php
@@ -7,10 +7,12 @@ class HttpCode {
     const INVALID = 400;
     const UNAUTHORISED = 401;
     const FORBIDDEN = 403;
-    const SERVER_ERROR = 500;
     const NOT_FOUND = 404;
+    const NOT_ACCEPTABLE = 406;
     const CONFLICT = 409;
     const CANNOT_PROCESS = 422;
+
+    const SERVER_ERROR = 500;
 
     const MOVED_PERMANENTLY = 301;
     const MOVED_TEMPORARILY = 302;
