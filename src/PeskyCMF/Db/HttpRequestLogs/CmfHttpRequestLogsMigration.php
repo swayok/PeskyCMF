2d4c715bb4452c38f0682a59d8ff18b5e9359501
Alexander Filippov <swayalex@gmail.com>
2018-09-30 14:09:48 +0300

    CmfHttpRequestLogsMigration - updated id column to be int8

--- src/PeskyCMF/Db/HttpRequestLogs/CmfHttpRequestLogsMigration.php
+++ src/PeskyCMF/Db/HttpRequestLogs/CmfHttpRequestLogsMigration.php
@@ -4,14 +4,13 @@ namespace PeskyCMF\Db\HttpRequestLogs;
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use PeskyCMF\Db\Admins\CmfAdminsTable;
 
 class CmfHttpRequestLogsMigration extends Migration {
 
     public function up() {
         if (!\Schema::hasTable(CmfHttpRequestLogsTableStructure::getTableName())) {
             \Schema::create(CmfHttpRequestLogsTableStructure::getTableName(), function (Blueprint $table) {
-                $table->increments('id');
+                $table->bigIncrements('id');
                 $table->integer('requester_id')->unsigned()->nullable();
                 $table->string('requester_table')->nullable();
                 $table->string('requester_info')->nullable();
@@ -29,8 +28,7 @@ class CmfHttpRequestLogsMigration extends Migration {
                 $table->integer('item_id')->unsigned()->nullable();
                 $table->json('data_before')->nullable();
                 $table->json('data_after')->nullable();
-                $currentTimestamp = \DB::raw(CmfAdminsTable::quoteDbExpr(CmfAdminsTable::getCurrentTimeDbExpr()->setWrapInBrackets(false)));
-                $table->timestampTz('created_at')->default($currentTimestamp);
+                $table->timestampTz('created_at')->default(\DB::raw('NOW()'));
                 $table->timestampTz('responded_at')->nullable();
 
                 $table->index('response_code');
