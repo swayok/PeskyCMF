a16a78f54352a65066d3ce59767056f378da9fb7
Alexander Filippov <swayalex@gmail.com>
2020-05-25 10:42:16 +0300

    about.php - updated links

--- src/PeskyCMF/resources/views/page/about.php
+++ src/PeskyCMF/resources/views/page/about.php
@@ -3,8 +3,11 @@
         <h1 class="text-center">About PeskyCMF</h1>
         <h2>Things I used for free</h2>
         <ul>
-            <li><a href="https://almsaeedstudio.com/">AdminLTE Template by Almsaeed Studio</a></li>
-            <li>Mosquito icon by Alex Auda Samora from the Noun Project</li>
+            <li><a href="https://almsaeedstudio.com/" target="_blank">AdminLTE Template by Almsaeed Studio</a></li>
+            <li>
+                <a href="https://thenounproject.com/term/mosquito/34825/" target="_blank">Mosquito icon</a>
+                by <a href="https://thenounproject.com/audasamora/" target="_blank">AS Design</a> from the Noun Project</a>
+            </li>
         </ul>
     </div>
 </div>
