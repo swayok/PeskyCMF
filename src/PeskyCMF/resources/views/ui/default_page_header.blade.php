ca03d1ae673382ce646951bd3b7f0883633a3e50
Alexander Filippov <swayalex@gmail.com>
2020-06-19 15:16:59 +0300

    default_page_header.blade.php - added stick-to-top option

--- src/PeskyCMF/resources/views/ui/default_page_header.blade.php
+++ src/PeskyCMF/resources/views/ui/default_page_header.blade.php
@@ -3,9 +3,13 @@
  * @var string $header
  * @var bool|null $addBackUrl
  * @var string|null $defaultBackUrl
+ * @var bool|null $stickToTop
  */
 ?>
-<div class="content-header">
+<div class="content-header {{ $stickToTop ? 'stick-to-top' : '' }}">
     <h1>{!! $header !!}</h1>
     @include('cmf::ui.default_breadcrumbs')
 </div>
+@if ($stickToTop)
+    <div class="sticked-content-header-placeholder"></div>
+@endif
